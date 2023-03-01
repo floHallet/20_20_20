9d5582c60e05d5d8201867d7cac52474d108b1d1
floHal <flohal@gmail.com>
2023-03-01 19:22:04 +0100

    mvp

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import { Clock } from './Clock/Clock'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <Clock></Clock>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Clock/Clock.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+
+export function Clock() {
+    const [startTime, setStartTime] = useState(0);
+    const audio = new Audio("/cat.mp3");
+    const timeToStop = 20 * 60 * 1000; // 20 minutes
+
+    /*useEffect(() => {
+      //document.title = `${new Date().toLocaleTimeString()}`
+    })*/
+
+    useEffect(()=>{
+      const interval = setInterval(() => {
+        //console.log('interval called');
+        checkElapsedTimeSinceStart();
+      }, 1000);
+      return ()=>{
+        clearInterval(interval);
+        //console.log('interval cleared');
+      }
+    }, [startTime]);
+
+    function checkElapsedTimeSinceStart() {
+      //console.log('checkElapsedTimeSinceStart fn called');
+      if (!startTime) return;
+      const timeElapsed = Date.now() - startTime;
+      document.title = Math.floor(timeElapsed/1000)
+      if (timeElapsed >= timeToStop) {
+        audio.play();
+        setTimeout(() => {
+          audio.pause();
+          setStartTime(0);
+        }, 20000);
+      }
+    }
+
+    function startTimer() {
+      //console.log('StartOver fn called');
+      setStartTime(Date.now());
+    }
+  
+    return (
+      <>
+        <button onClick={startTimer} disabled={startTime}>
+            {startTime ? `counting... !` : 'Start !'}
+        </button>
+      </>
+    )
+}
